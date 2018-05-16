6f11e4f98a8db3f31c656aedc537710369274912
Daksha Patel <dakshapatel100@gmail.com>
2018-05-16 13:28:12 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -5,7 +5,7 @@ class Teacher < User
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
   KNOWLEDGE = @knowledge_array
 
-def teach(knowledge_array)
+def teach
   knowledge.teach
   #knowledge_array.sample
 
